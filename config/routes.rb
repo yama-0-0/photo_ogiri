bd0e02757ab8d5480cc944e2ca8358de3876164e
taka president <life.i.s.w---@i.softbank.jp>
2020-01-18 07:56:57 +0900

    投稿削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
-  resources :posts, only: %i(new create index show) do
+  resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
   end
 end
